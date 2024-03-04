553529871e365b39c81157579139e6173d58ee29
Abdallahaweda <abdallahslahaweda@gmail.com>
2024-03-03 17:14:46 -0800

    aweda

---
+++ public/index.js
@@ -0,0 +1,38 @@
+let socket =io("http://localhost:3500")
+
+const activity=document.querySelector(".activity")
+const msgInput=document.querySelector("input")
+
+function sendMessage (e){
+    e.preventDefault()
+    
+    if(msgInput.value){
+        socket.emit('message',msgInput.value)
+        msgInput.value=""
+    }
+    msgInput.focus()
+}
+document.querySelector('form').addEventListener('submit',sendMessage)
+
+//listen for msg
+socket.on('message',(data)=>{
+    activity.textContent=''
+    let li=document.createElement('li')
+    li.textContent=data
+    document.querySelector('ul').appendChild(li)
+})
+
+msgInput.addEventListener('keypress',()=>{
+    socket.emit('activity',socket.id.substring(0,5))
+})
+
+//we will receive a name from back-end
+let activityTimer
+socket.on('activity',(name)=>{
+    activity.textContent=`${name} is Typing...`
+    //Clear after 1sec
+    clearTimeout(activityTimer)
+    activityTimer=setTimeout(()=>{
+        activity.textContent=''
+    },2000)
+})
